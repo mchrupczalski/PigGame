9416261ccde41c0a02ce8c25a9aac50d62a70cc2
Mateusz Chrupczalski <m.chrupczalski@outlook.com>
2022-09-20 20:50:53 +0100

    Adding Validators tests

---
+++ PigGame.lib.tests/Validators/SingleDiceRollValidatorTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using PigGame.lib.Exceptions;
+using PigGame.lib.Validators;
+using Xunit;
+
+namespace PigGame.lib.tests.Validators
+{
+    public class SingleDiceRollValidatorTests
+    {
+        [Fact]
+        public void RollIsValid_OnePassed_ThrowsException()
+        {
+            var validator = SingleDiceRollValidatorDefaultConstructor();
+            var rolls = new List<int>() { 1 };
+            Assert.Throws<OneRolledNoScoreTurnEndException>(() => validator.RollIsValid(rolls));
+        }
+
+        [Fact]
+        public void RollIsValid_HigherThanOnePassed_ReturnsTrue()
+        {
+            var validator = SingleDiceRollValidatorDefaultConstructor();
+            var rolls = new List<int>() { 2 };
+            Assert.True(validator.RollIsValid(rolls));
+        }
+        
+        private static SingleDiceRollValidator SingleDiceRollValidatorDefaultConstructor() => new SingleDiceRollValidator();
+    }
+}
---
+++ PigGame.lib.tests/Validators/TwoDiceRollValidatorTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using PigGame.lib.Exceptions;
+using PigGame.lib.Validators;
+using Xunit;
+
+namespace PigGame.lib.tests.Validators
+{
+    public class TwoDiceRollValidatorTests
+    {
+        [Theory]
+        [InlineData(1, 2)]
+        [InlineData(2, 1)]
+        public void RollIsValid_NoDoublesAtLeastOneOnePassed_ThrowsException(int rollOne, int rollTwo)
+        {
+            var validator = TwoDiceRollValidatorDefaultConstructor(false);
+            var rolls = new List<int>() { rollOne, rollTwo };
+            Assert.Throws<OneRolledNoScoreTurnEndException>(() => validator.RollIsValid(rolls));
+        }
+        
+        [Fact]
+        public void RollIsValid_NoDoublesThreeValuesPassed_ThrowsException()
+        {
+            var validator = TwoDiceRollValidatorDefaultConstructor(false);
+            var rolls = new List<int>() { 4, 2, 3 };
+            Assert.Throws<ArgumentOutOfRangeException>(() => validator.RollIsValid(rolls));
+        }
+        
+        [Fact]
+        public void RollIsValid_NoDoublesTwoOnesPassed_ThrowsException()
+        {
+            var validator = TwoDiceRollValidatorDefaultConstructor(true);
+            var rolls = new List<int>() { 1, 1 };
+            Assert.Throws<DoubleOnesRolledScoreLostTurnEndException>(() => validator.RollIsValid(rolls));
+        }
+        
+        private static TwoDiceRollValidator TwoDiceRollValidatorDefaultConstructor(bool doubles) => new TwoDiceRollValidator(doubles);
+    }
+}
